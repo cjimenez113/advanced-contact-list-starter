1965dda6e97b8ecca07434eef545acc177afb8ea
cjimenez113 <christopherjimenez@Christophers-MacBook-Air.local>
2017-01-25 00:21:59 -0600

    components and states

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+/* eslint-disable max-len*/
 import React, { Component } from 'react';
-
+import ContactList from './ContactList';
+import SearchBar from './SearchBar';
 class App extends Component {
   render() {
+
     return (
       <div className="App">
         <h1>
-          Contact List!
-        </h1>
+        Contacts list!
+      </h1>
+        <SearchBar />
+        <ContactList />
       </div>
     );
   }
